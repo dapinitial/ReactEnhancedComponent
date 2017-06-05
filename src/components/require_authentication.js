2811be6160bb5eccbb79975598e6dea3b3a9866f
David Puerto <me@davidpuerto.com>
2017-06-05 01:41:15 -0700

    adding static contextTypes for authn

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
     render() {
+      //console.log('Rendering ', ComposedComponent)
+      console.log(this.context);
+      console.log(this.props.authenticated);
       return <ComposedComponent {...this.props} />
     }
   }
-  return Authentication;
+
+  function mapStateToProps(state) {
+    return { authenticated: state.authenticated }
+  }
+
+  return connect(mapStateToProps)(Authentication);
 }
