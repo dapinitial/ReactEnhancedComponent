77465ad051a44a78c431eb63410191e6fe940f93
David Puerto <me@davidpuerto.com>
2017-06-05 01:24:15 -0700

    scaffolding authentication for the higher order component

---
+++ src/actions/types.js
@@ -0,0 +1,3 @@
+export const CHANGE_AUTH = 'change_auth';
+
+
---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+export default () => {
+  return (
+    <div>Super Secret Hiding Spots!
+      <ul>
+        <li>Hiding Spot #1</li>
+        <li>Hiding Spot #2</li>
+        <li>Hiding Spot #3</li>
+      </ul>
+    </div>
+  );
+}
